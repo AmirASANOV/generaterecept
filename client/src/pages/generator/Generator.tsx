d48ff0dd9c7bb931ddec78225fb661439c565b41
Akzakoins <emirsasanov@gmail.com>
2024-07-16 00:56:05 +0300

    added bg

--- client/src/pages/generator/Generator.tsx
+++ client/src/pages/generator/Generator.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Input from "../../components/Input/Input";
+import s from "./Generator.module.scss";
 
 const Generator = () => {
   return (
-    <div>
+    <div className={s.wrapper}>
       <Input />
     </div>
   );

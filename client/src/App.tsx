a6ccd587c8bd331b2ef742c3efc74b03fb1ab504
Akzakoins <emirsasanov@gmail.com>
2024-07-16 00:41:56 +0300

    deleted video

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import s from "./App.module.scss";
 function App() {
   return (
     <div className={s.wrapper}>
-      <video src="bg.mp4" autoPlay muted loop id="myVideo" />
       <Generator />
     </div>
   );
